befbb9c41b86712d01fa15a71978dde63a1db3e7
Cyber-Kira <kkoloskov02@gmail.com>
2019-12-17 20:17:52 +0300

    add game-even func

--- src/index.js
+++ src/index.js
@@ -2,8 +2,40 @@
 
 import readLineSync from 'readline-sync';
 
-const askingName = () => {
+export const greetings = () => console.log('Welcome to the Brain Games!');
+
+export const askingName = () => {
   const userName = readLineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
+  console.log(`Hello, ${userName}!\n`);
+  return userName;
 };
-export default askingName;
+
+export const descrForGameEven = () => {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".\n');
+};
+
+export const gameEven = (userName) => {
+  let counter = 0;
+  let correctAnswer;
+  while (counter < 3) {
+    const rndNumber = Math.floor(Math.random() * 50);
+    console.log(`Question: ${rndNumber}`);
+    const answer = readLineSync.question('Your answer: ');
+    const isEven = rndNumber % 2 === 0;
+    if (isEven) {
+      correctAnswer = 'yes';
+    } else {
+      correctAnswer = 'no';
+    }
+    if ((answer === 'yes' && isEven) || (answer === 'no' && isEven === false)) {
+      console.log('Correct!');
+      counter += 1;
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      break;
+    }
+  }
+  if (counter === 3) {
+    console.log(`Congratulations, ${userName}!`);
+  }
+};
