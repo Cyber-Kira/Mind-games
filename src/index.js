befe1a24ad73d2a3d30a2e23ff28742e2680663d
Cyber-Kira <kkoloskov02@gmail.com>
2020-01-03 14:51:09 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -128,22 +128,30 @@ export const gameGCD = (userName) => {
   }
 };
 
+const getProgression = () => {
+  let hiddenElem = '';
+  let progression = '';
+  let numOfProgression = getRngInt(0, 10);
+  const step = getRngInt(1, 4);
+  const indexOfHiddenElem = getRngInt(0, 10);
+  for (let i = 0; i < 10; i += 1, numOfProgression += step) {
+    if (indexOfHiddenElem === i) {
+      hiddenElem = String(numOfProgression);
+      progression += '.. ';
+    } else {
+      progression += `${String(numOfProgression)} `;
+    }
+  }
+  const pair = cons(progression, hiddenElem);
+  return pair;
+};
+
 export const gameProgression = (userName) => {
   let counter = 0;
   while (counter < 3) {
-    let hiddenElem = '';
-    let progression = '';
-    let numOfProgression = getRngInt(0, 10);
-    const step = getRngInt(1, 4);
-    const indexOfHiddenElem = getRngInt(0, 10);
-    for (let i = 0; i < 10; i += 1, numOfProgression += step) {
-      if (indexOfHiddenElem === i) {
-        hiddenElem = numOfProgression;
-        progression += '.. ';
-      } else {
-        progression += `${String(numOfProgression)} `;
-      }
-    }
+    const progressionPair = getProgression();
+    const progression = car(progressionPair);
+    const hiddenElem = cdr(progressionPair);
     console.log(`Question: ${progression}`);
     const userAnswer = readLineSync.question('Your answer: ');
     const correctAnswer = hiddenElem;
