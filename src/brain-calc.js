e9d680734e1674ba59743d74874a436072a226d1
Cyber-Kira <kkoloskov02@gmail.com>
2020-03-16 16:27:15 +0300

    mv packages

--- src/brain-calc.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-import gameCalc from './bin/games/gameCalc';
-
-gameCalc();
--- src/brain-even.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-import gameEven from './bin/games/gameEven';
-
-gameEven();
