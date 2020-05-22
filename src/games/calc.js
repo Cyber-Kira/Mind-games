1287280f66e5ddd5ba9d52ae63cb44801ecf4890
Cyber-Kira <kkoloskov02@gmail.com>
2020-05-22 16:51:50 +0300

    changed names

--- src/games/calc.js
+++ src/games/calc.js
@@ -6,8 +6,8 @@ const description = 'What is the result of the expression?';
 
 const signs = ['+', '-', '*'];
 
-const calculate = (Sign, firstNumber, secondNumber) => {
-  switch (Sign) {
+const calculate = (sign, firstNumber, secondNumber) => {
+  switch (sign) {
     case '+':
       return firstNumber + secondNumber;
     case '-':
@@ -22,9 +22,10 @@ const calculate = (Sign, firstNumber, secondNumber) => {
 const getGameData = () => {
   const firstNumber = getRandomInt(0, 10);
   const secondNumber = getRandomInt(0, 10);
-  const Sign = signs[getRandomInt(0, 2)];
-  const question = (`${firstNumber} ${Sign} ${secondNumber}`);
-  const correctAnswer = calculate(Sign, firstNumber, secondNumber);
+  const indexOfLastSign = signs.length - 1;
+  const sign = signs[getRandomInt(0, indexOfLastSign)];
+  const question = (`${firstNumber} ${sign} ${secondNumber}`);
+  const correctAnswer = calculate(sign, firstNumber, secondNumber);
   return cons(question, String(correctAnswer));
 };
 
--- src/games/progression.js
+++ src/games/progression.js
@@ -1,11 +1,8 @@
 import {
   cons,
-  car,
-  cdr,
 } from '@hexlet/pairs';
 import getRandomInt from '../utils';
 import startGame from '..';
-
 
 const description = 'What number is missing in the progression?';
 
@@ -23,20 +20,13 @@ const createQuestion = (firstElement, lastElement, indexOfHiddenElement, step) =
   return iter(firstElement, 1, '');
 };
 
-const getQandA = (firstElement, step, index, progressionLength) => {
-  const question = createQuestion(firstElement, progressionLength, index, step);
-  const answer = firstElement + step * (index - 1);
-  return cons(question, answer);
-};
-
 const getGameData = () => {
   const progressionLength = 10;
   const firstElement = getRandomInt(0, 10);
   const step = getRandomInt(1, 4);
   const indexOfHiddenElement = getRandomInt(1, progressionLength);
-  const QandA = getQandA(firstElement, step, indexOfHiddenElement, progressionLength);
-  const question = car(QandA);
-  const answer = cdr(QandA);
+  const question = createQuestion(firstElement, progressionLength, indexOfHiddenElement, step);
+  const answer = firstElement + step * (indexOfHiddenElement - 1);
   return cons(question, String(answer));
 };
 
