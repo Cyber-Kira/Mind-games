edef536727027d2d5c5ffa1ae4a2b3b6b795439d
Cyber-Kira <kkoloskov02@gmail.com>
2020-05-22 19:29:32 +0300

    createQuestion refactoring

--- src/games/progression.js
+++ src/games/progression.js
@@ -8,7 +8,7 @@ const description = 'What number is missing in the progression?';
 
 const createQuestion = (firstElement, lastElement, indexOfHiddenElement, step) => {
   const iter = (number, counter, acc) => {
-    if (counter > lastElement) {
+    if (counter === lastElement) {
       return acc.trim();
     }
     if (counter === indexOfHiddenElement) {
@@ -17,7 +17,7 @@ const createQuestion = (firstElement, lastElement, indexOfHiddenElement, step) =
     return iter(number + step, counter + 1, `${acc} ${number}`);
   };
 
-  return iter(firstElement, 1, '');
+  return iter(firstElement, 0, '');
 };
 
 const getGameData = () => {
@@ -26,7 +26,7 @@ const getGameData = () => {
   const step = getRandomInt(1, 4);
   const indexOfHiddenElement = getRandomInt(1, progressionLength);
   const question = createQuestion(firstElement, progressionLength, indexOfHiddenElement, step);
-  const answer = firstElement + step * (indexOfHiddenElement - 1);
+  const answer = firstElement + step * indexOfHiddenElement;
   return cons(question, String(answer));
 };
 
